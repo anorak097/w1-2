0954afe7be7efe9c14da6593479ba90ca51f8152
Anorak097 <anorakyang@gmail.com>
2026-08-10 15:26:02 +0800

    first commit

---
+++ node_modules/slidev-theme-the-unnamed/setup/shiki.ts
@@ -0,0 +1,9 @@
+/* ./setup/shiki.ts */
+import { defineShikiSetup } from "@slidev/types";
+import theme from "../public/theme/theunnamed-dark-theme.json";
+
+export default defineShikiSetup(() => {
+  return {
+    theme,
+  };
+});
