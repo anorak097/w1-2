b4ba59dc75311216ee404989f94cd2c4c2a00352
Anorak097 <anorakyang@gmail.com>
2026-09-15 20:27:14 +0800

    feat: remove deprecated Vite dependencies and add global download button
    - Deleted unused Vite dependency files related to node:fs and node:https for browser compatibility.
    - Removed rolldown-runtime and its associated files to streamline the build process.
    - Added Playwright Chromium as a dev dependency for testing purposes.
    - Introduced a new global bottom component to provide a download link for the presentation PDF on the first slide.
    - Updated slides to remove unnecessary content and improve clarity.

---
+++ global-bottom.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { useNav } from '@slidev/client'
+
+// 從 useNav() 中取出 currentPage, currentLayout 與頁面跳轉方法 go
+const { currentPage, currentLayout, go } = useNav()
+
+// 定義導向目錄頁的方法
+const tocPage = () => {
+  go('Outline') // 直接傳入投影片別名 (routeAlias)
+}
+</script>
+
+<template>
+    <div 
+        v-if="currentPage == 1"
+        class="fixed bottom-2 right-2 z-50 flex gap-2 items-center"
+    >
+      <span style="font-size: 1.8rem;" class="text-gray-300">下載簡報</span>    
+      <a 
+        href="https://raw.githubusercontent.com/anorak097/w1-1/main/%231%20Overview%20-%20Slidev.pdf"
+        target="_blank"
+        download
+        class="inline-flex items-center justify-center p-2 rounded-full bg-gray-400 bg-opacity-10 hover:bg-opacity-20 border border-gray-500 border-opacity-20 backdrop-blur-md transition-all active:scale-90 !text-current"
+        title="Download Slidev PDF"
+      >
+        <lucide:download class="text-xl opacity-60" />
+      </a>
+    </div>
+</template>
